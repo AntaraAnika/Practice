037d602c5bd2ce8b8495c9ac2c8d01385e757a01
AntaraAnika <anika@insurecow.com>
2022-04-24 14:54:35 +0600

    php

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index($id){
+        return view( 'user.user-view',compact('id'));
+    }
+
+
+}
+
+
+
+
+
